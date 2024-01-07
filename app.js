0c39b14aabbf9c2c4bca11c14137a6165f484aff
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2024-01-07 23:43:33 +0600

    admin js integrated

--- app.js
+++ app.js
@@ -3,38 +3,51 @@ require("express-async-errors");
 const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const compression = require("compression");
 const unknownEndpoint = require("./middleware/unKnownEndpoint");
 const { handleError } = require("./helpers/error");
+const { pool } = require("./db/config");
+
 
 const app = express();
 
-app.set("trust proxy", 1);
-app.use(cors({ credentials: true, origin: true }));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(compression());
-app.use(helmet());
-app.use("/uploads", express.static("uploads"));
+Promise.all([
+    import('adminjs'),
+    import('@adminjs/express'),
+]).then(([{ default: AdminJS }, { default: AdminJSExpress }]) => {
 
-// CORS handling
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+    app.set("trust proxy", 1);
+    app.use(cors({ credentials: true, origin: true }));
+    app.use(express.json());
+    app.use(morgan("dev"));
+    app.use(compression());
+    // app.use(helmet());
+    app.use("/uploads", express.static("uploads"));
+
+    // CORS handling
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        if (req.method === "OPTIONS") {
+            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    app.use(`/api`, routes);
+
+    const adminJS = new AdminJS({});
+    const adminRouter = AdminJSExpress.buildRouter(adminJS)
+    app.use(adminJS.options.rootPath, adminRouter)
+
+    app.use('*', unknownEndpoint);
+    app.use(handleError);
 });
 
-app.use(`/api`, routes);
-
-app.use('*', unknownEndpoint);
-app.use(handleError);
-
 module.exports = app;
+
--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const IP = process.env.IP || `0.0.0.0`;
 
 server.listen(PORT, IP, () =>
     logger.info(
-        `Server is running (${process.env.NODE_ENV}) on ip: ${IP} and port: ${PORT}.\n >> Visit http://localhost:${PORT}/api/health/\nPlease press CTRL + C to stop the server`,
+        `Server is running (${process.env.NODE_ENV}) on ip: ${IP} and port: ${PORT}.\n>> Visit: http://localhost:${PORT}/api/health/\n>> Admin: http://localhost:${PORT}/admin/\nPlease press CTRL + C to stop the server`,
     ),
 );
+
+
+// const express = require('express');
+
+// Promise.all([
+//     import('adminjs'),
+//     import('@adminjs/express'),
+// ]).then(([{ default: AdminJS }, { default: AdminJSExpress }]) => {
+//     const PORT = 3000
+//     const app = express()
+
+//     const admin = new AdminJS({})
+
+//     const adminRouter = AdminJSExpress.buildRouter(admin)
+//     app.use(admin.options.rootPath, adminRouter)
+
+//     app.listen(PORT, () => {
+//         console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+//     })
+// });
