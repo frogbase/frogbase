79cbe33550a53f059d61cb438ae08642d9a41daf
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2024-01-09 03:40:17 +0600

    es-module merge

--- app.mjs
+++ app.mjs
@@ -18,12 +18,7 @@ app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(compression());
-<<<<<<< HEAD:app.js
-// app.use(helmet());
-app.use("/uploads", express.static("uploads"));
-=======
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
->>>>>>> 78097b7490017c6a7b3c738a9ab6afa9225d048c:app.mjs
 
 // CORS handling
 app.use((req, res, next) => {
@@ -42,84 +37,7 @@ app.use((req, res, next) => {
 app.use(`/api`, routes);
 app.use('/admin', adminRouter);
 
-Promise.all([
-    import('adminjs'),
-    import('@adminjs/express'),
-    import('connect-pg-simple'),
-    import('express-session'),
-    import('@adminjs/sql'),
-]).then(async ([
-    { default: AdminJS },
-    { default: AdminJSExpress },
-    { default: Connect },
-    { default: session },
-    { default: { Adapter, Resource, Database } },
-]) => {
-    const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+app.use('*', unknownEndpoint);
+app.use(handleError);
 
-    AdminJS.registerAdapter({ Database, Resource })
-
-    const db = await new Adapter('postgresql', {
-        connectionString: connectionString,
-        database: process.env.POSTGRES_DB,
-    }).init();
-
-    const DEFAULT_ADMIN = { email: 'admin@algoramming.com', password: '@Admin123', name: 'FrogBase Admin' }
-
-    const authenticate = async (email, password) => {
-        if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) return Promise.resolve(DEFAULT_ADMIN)
-        return null
-    }
-
-    const adminJS = new AdminJS({
-        version: { admin: true, app: process.env.PROJECT_VERSION },
-        rootPath: "/admin",
-        databases: [db],
-    });
-    const ConnectSession = Connect(session)
-    const sessionStore = new ConnectSession({
-        conObject: {
-            connectionString: connectionString,
-            /*
-            SSL is not supported in development
-            */
-            ssl:
-                process.env.NODE_ENV === "production"
-                    ? { rejectUnauthorized: false }
-                    : false,
-        },
-        tableName: 'adminjs',
-        createTableIfMissing: true,
-    });
-    const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
-        adminJS,
-        {
-            authenticate,
-            cookieName: process.env.COOKIE_NAME,
-            cookiePassword: process.env.COOKIE_SECRET,
-        },
-        null,
-        {
-            store: sessionStore,
-            resave: true,
-            saveUninitialized: true,
-            secret: process.env.COOKIE_SECRET,
-            cookie: {
-                httpOnly: process.env.NODE_ENV === 'production',
-                secure: process.env.NODE_ENV === 'production',
-            },
-            name: process.env.COOKIE_NAME,
-        }
-    )
-    app.use(adminJS.options.rootPath, adminRouter)
-
-    app.use('*', unknownEndpoint);
-    app.use(handleError);
-});
-
-<<<<<<< HEAD:app.js
-module.exports = app;
-
-=======
 export default app;
->>>>>>> 78097b7490017c6a7b3c738a9ab6afa9225d048c:app.mjs
