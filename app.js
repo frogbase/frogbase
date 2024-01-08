2bb43c3a333c9a4fddb6675feba74decdcb574fc
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2024-01-08 11:21:56 +0600

    WIP

--- app.js
+++ app.js
@@ -12,37 +12,99 @@ const { pool } = require("./db/config");
 
 const app = express();
 
+app.set("trust proxy", 1);
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(compression());
+// app.use(helmet());
+app.use("/uploads", express.static("uploads"));
+
+// CORS handling
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use(`/api`, routes);
+
 Promise.all([
     import('adminjs'),
     import('@adminjs/express'),
-]).then(([{ default: AdminJS }, { default: AdminJSExpress }]) => {
+    import('connect-pg-simple'),
+    import('express-session'),
+    import('@adminjs/sql'),
+]).then(async ([
+    { default: AdminJS },
+    { default: AdminJSExpress },
+    { default: Connect },
+    { default: session },
+    { default: { Adapter, Resource, Database } },
+]) => {
+    const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
 
-    app.set("trust proxy", 1);
-    app.use(cors({ credentials: true, origin: true }));
-    app.use(express.json());
-    app.use(morgan("dev"));
-    app.use(compression());
-    // app.use(helmet());
-    app.use("/uploads", express.static("uploads"));
+    AdminJS.registerAdapter({ Database, Resource })
 
-    // CORS handling
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        if (req.method === "OPTIONS") {
-            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-            return res.status(200).json({});
-        }
-        next();
+    const db = await new Adapter('postgresql', {
+        connectionString: connectionString,
+        database: process.env.POSTGRES_DB,
+    }).init();
+
+    const DEFAULT_ADMIN = { email: 'admin@algoramming.com', password: '@Admin123', name: 'FrogBase Admin' }
+
+    const authenticate = async (email, password) => {
+        if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) return Promise.resolve(DEFAULT_ADMIN)
+        return null
+    }
+
+    const adminJS = new AdminJS({
+        version: { admin: true, app: process.env.PROJECT_VERSION },
+        rootPath: "/admin",
+        databases: [db],
     });
-
-    app.use(`/api`, routes);
-
-    const adminJS = new AdminJS({});
-    const adminRouter = AdminJSExpress.buildRouter(adminJS)
+    const ConnectSession = Connect(session)
+    const sessionStore = new ConnectSession({
+        conObject: {
+            connectionString: connectionString,
+            /*
+            SSL is not supported in development
+            */
+            ssl:
+                process.env.NODE_ENV === "production"
+                    ? { rejectUnauthorized: false }
+                    : false,
+        },
+        tableName: 'adminjs',
+        createTableIfMissing: true,
+    });
+    const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+        adminJS,
+        {
+            authenticate,
+            cookieName: process.env.COOKIE_NAME,
+            cookiePassword: process.env.COOKIE_SECRET,
+        },
+        null,
+        {
+            store: sessionStore,
+            resave: true,
+            saveUninitialized: true,
+            secret: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: process.env.NODE_ENV === 'production',
+                secure: process.env.NODE_ENV === 'production',
+            },
+            name: process.env.COOKIE_NAME,
+        }
+    )
     app.use(adminJS.options.rootPath, adminRouter)
 
     app.use('*', unknownEndpoint);
