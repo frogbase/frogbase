7cfd5d6f3effe51aedc77be82fa160e923b58903
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2024-01-22 02:23:17 +0600

    minor change

--- admin.js
+++ admin.js
@@ -6,7 +6,7 @@ const admin = async (_, res) => {
     try {
         const users = (await pool.query('SELECT * FROM users ORDER BY created ASC'));
         const posts = (await pool.query('SELECT * FROM posts ORDER BY created ASC'));
-        const logs = (await pool.query('SELECT * FROM logs ORDER BY created ASC'));
+        const logs = (await pool.query('SELECT * FROM logs ORDER BY created DESC'));
 
         res.render('admin', { data: { users, posts, logs } });
     } catch (error) {
